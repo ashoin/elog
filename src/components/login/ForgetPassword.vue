255b54e1cacfb05a72570f37f5a5d723a515f3d9
ashoin <56584166+ashoin@users.noreply.github.com>
2020-10-26 22:08:20 +0800

    Revert "Revert "10.26""
    This reverts commit 9a92e63382c85ba5b8a2552d91d517dc2237609c.

---
+++ src/components/login/ForgetPassword.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="forget-password-container">
+    <!-- 头部导航信息nav -->
+    <div class="forget-password-head">
+      <van-nav-bar @click-left="onClickLeft">
+        <van-icon name="arrow-left" slot="left" />
+        <h3 slot="title">忘记密码</h3>
+      </van-nav-bar>
+    </div>
+    <!-- 主体部分内容 v-if -->
+    <div class="forget-password-main">
+      <van-cell-group v-if="isShow">
+        <van-field
+          class="input"
+          v-model="mobile"
+          center
+          clearable
+          border
+          type="number"
+          size="large"
+          autosize=""
+          placeholder="请输入手机号码"
+        />
+        <van-field
+          class="input"
+          v-model="sms"
+          center
+          clearable
+          placeholder="请输入验证码"
+        >
+          <van-button slot="button" size="small" type="primary">
+            获取验证码
+          </van-button>
+        </van-field>
+      </van-cell-group>
+      <van-cell-group v-else>
+        <van-field
+          class="input"
+          v-model="mobile"
+          center
+          clearable
+          border
+          type="number"
+          size="large"
+          autosize=""
+          placeholder="请输入新密码"
+        />
+        <van-field
+          class="input"
+          v-model="sms"
+          center
+          clearable
+          placeholder="请再次输入新密码"
+        >
+        </van-field>
+      </van-cell-group>
+      <!-- 底部按钮转换 isShow -->
+      <van-row>
+        <van-button
+          size="large"
+          style="width: 60%; margin: 20px 0"
+          color="#D41C1C"
+          @click="nextStep"
+        >
+          {{ next }}
+        </van-button>
+      </van-row>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      mobile: "",
+      sms: "",
+      isShow: true,
+      next: "下一步",
+    };
+  },
+
+  methods: {
+    onClickLeft() {
+      this.$router.push("/login");
+    },
+    nextStep() {
+      if (this.count == 0) {
+        this.isShow = false;
+        this.next = "确认修改";
+        this.count++;
+      } else if (this.count == 1) {
+        this.next = "下一步";
+        this.count = 0;
+        this.isShow = true;
+        console.log("goooooooooooood");
+      }
+    },
+  },
+};
+</script>
+<style scoped>
+.input {
+  height: 50px;
+  width: 100%;
+  font-size: 18px;
+  margin: 20px 0;
+  border: 1px solid silver;
+}
+
+.forget-password-main {
+  width: 80%;
+  margin: 20px auto;
+}
+</style>
